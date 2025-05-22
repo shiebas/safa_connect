430633336514655ecfc94e5345609837a4ef8ec3
shiebas <shaunqjohannes@gmail.com>
2025-05-22 02:03:26 +0200

    worldsports 7

--- geography/views.py
+++ geography/views.py
@@ -2,11 +2,32 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from .models import WorldSportsBody
+from .models import (
+    Country, Province, Region, WorldSportsBody, Continent,
+    ContinentFederation, Club, Association, Membership
+)
+from django.shortcuts import render
+import datetime
+from django.views.generic import CreateView
 
+# Advanced global home page
+def advanced_home(request):
+    countries = Country.objects.all().order_by('name')
+    return render(request, "home.html", {
+        "countries": countries,
+        "year": datetime.datetime.now().year,
+    })
+
+# Admin dashboard view for the geography app
+@login_required
+def geography_admin(request):
+    return render(request, "geography/geography_admin.html")
+
+# Decorator for all CBVs
 login_decorator = method_decorator(login_required, name='dispatch')
 
-@login_decorator
+
+ @login_decorator
 class WorldSportsBodyListView(ListView):
     model = WorldSportsBody
     template_name = 'geography/worldsportsbody_list.html'
@@ -36,4 +57,5 @@ class WorldSportsBodyUpdateView(UpdateView):
 class WorldSportsBodyDeleteView(DeleteView):
     model = WorldSportsBody
     template_name = 'geography/worldsportsbody_confirm_delete.html'
-    success_url = reverse_lazy('geography:worldsportsbody-list')
+    success_url = reverse_lazy('geography:worldsportsbody-list') 
+
